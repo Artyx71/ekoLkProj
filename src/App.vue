4dc82bf724a9fbeb8be58c41d7ec9ab18c371cf5
Andrew Gabaraev <androgabar@gmail.com>
2024-12-19 21:37:47 +0300

    215421

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,4 @@
-<script setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-
-const password = ref('')
-const inn = ref('')
-const router = useRouter()
-const isRemembered = ref(false)
-const handleLogin = () => {
-	if (inn.value === '123' && password.value === '123') {
-		router.push('/home')
-	} else {
-		console.log(inn.value)
-		console.log(password.value)
-		alert('Неверный логин или пароль')
-	}
-}
-const logInput = (fieldName, value) => {
-	console.log(`Поле ${fieldName} обновлено:`, value)
-}
-</script>
+<script setup></script>
 
 <template>
 	<router-view></router-view>
